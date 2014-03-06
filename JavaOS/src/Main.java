b4617821cb0fc190aa4192ce729d2df0e0c76b3e
branmart <branmart@uw.edu>
2014-03-05 19:10:46 -0800

    Changed main

--- JavaOS/src/Main.java
+++ JavaOS/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 /*
  * @author Brandon Martin
@@ -10,6 +12,15 @@ public class Main {
     public static void main(String[] args) {
        //call timer to  start
        //ask user which type of scheduler
+       
+      
+        System.out.println("Which type of scheduler would you like to use?");
+        System.out.println("1) Round Robin");
+        System.out.println("2) Priority");
+        System.out.println("3) Lottery");
+        
+        Scanner s = new Scanner(System.in);
+        
 
     }
     
