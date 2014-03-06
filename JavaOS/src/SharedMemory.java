538cf9d73860f92f6b339bfaee2e577da0c5cfb9
albinopython <mrw024@uw.edu>
2014-03-05 19:22:18 -0800

    adsfadf

---
+++ JavaOS/src/SharedMemory.java
@@ -0,0 +1,14 @@
+
+public class SharedMemory
+{
+	private static final int CAPACITY = 100;
+	
+	private final int[] my_memory;
+	
+	public SharedMemory()
+	{
+		my_memory = new int[CAPACITY];
+	}
+	
+	
+}
