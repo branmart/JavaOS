ff3d8fc17e3ab5431065f39f71afb006e3782b9c
wickedking <cody.shafer@gmail.com>
2014-03-09 13:54:46 -0700

    Changed consumer so it compiles.

--- JavaOS/src/Consumer.java
+++ JavaOS/src/Consumer.java
@@ -1,8 +1,27 @@
 
 public class Consumer extends Process{
 
-	@Override
+	public Consumer(int the_start, int the_end, State the_state) {
+		super(the_start, the_end, the_state);
+		// TODO Auto-generated constructor stub
+	}
+
+	
 	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	protected void setUpCommands() {
 		// TODO Auto-generated method stub
 		
 	}
