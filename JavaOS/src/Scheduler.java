7c6d75b55fd1d77a2809e869e9238a9dd7bdc4ec
branmart <branmart@uw.edu>
2014-03-09 15:23:56 -0700

    Still working on shit

--- JavaOS/src/Scheduler.java
+++ JavaOS/src/Scheduler.java
@@ -1,4 +1,6 @@
+import java.util.List;
 import java.util.Observable;
+import java.util.Queue;
 
 /**
  * 
@@ -14,11 +16,34 @@ public class Scheduler {
     
     public boolean is_lottery;
     
+    /**
+     * Queue of processes
+     */
+    Queue<Process> my_process_q;
+    
+    /**
+     * List of jobs that are running
+     */
+    List <Process> my_run_list;
+    
+    /**
+     * List of jobs that are waiting
+     */
+    List <Process> my_waiting_list;
+    
+    /**
+     * List of jobs that are blocked
+     */
+    List <Process> my_blocked_list;
+
+    
+    
+    //TODO remove flags
     //TODO make a singleton
     //TODO implement the method next process for the cpu switching
     
     public Process nextProcess(final Observable the_interrupt){
-    	//TODO make this switch proccess. if no switching return null;
+    	//TODO make this tell which process is next process. if no switching return null;
     	return null;
     }
 
@@ -27,10 +52,8 @@ public class Scheduler {
      * Sets round robin flag to true and all others to false
      */
     public void setRoundRobin() {
-        is_round_robin = true;
-        is_priority = false;
-        is_lottery = false;
-        
+        is_round_robin = true;      
+        roundRobin();
     }
     
     /**
@@ -38,19 +61,55 @@ public class Scheduler {
      */
     public void setPriority() {
         is_priority = true;
-        is_lottery = false;
-        is_round_robin = false;
-        
+        priority();
     }
     
     /**
      * Set lottery flag true and all others to false
      */
-    public void setLottery() {
-        is_lottery = true;
-        is_round_robin = false;
-        is_priority = false;
+    public void setLottery() {       
+        is_lottery = true;        
+        lottery();
+    }
+    
+    /**
+     * Implementation of lottery
+     * 
+     */
+    private void lottery() {
+        // TODO insert code   
+    }
+
+
+    /**
+     * Implementation of Round Robin
+     */
+    private void roundRobin() {
+        Process current_processing_job = my_process_q.element(); //current job
         
+        /**
+         * makes it run forever
+         */
+        while (true) {
+            if(current_processing_job != null ) {
+                my_waiting_list.add(current_processing_job);
+                current_processing_job = null;
+            }
+            if(current_processing_job == null && my_waiting_list.size() != 0) {
+                current_processing_job = my_waiting_list.get(0);
+                
+            }
+            if(current_processing_job != null) {
+
+            }
+        }       
+    }
+    
+    /**
+     * Implementation of Priority
+     */
+    private void priority() {
+        //TODO insert code
     }
 
 }
